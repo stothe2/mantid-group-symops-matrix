19d99523aef05321ff2520a69a96babc4447ed59
Sachi Sanghavi <sachi@jhu.edu>
2016-01-21 14:32:43 -0500

    Update files

--- sgData/symops.py
+++ sgData/symops.py
@@ -1,13 +1,14 @@
-import sys, io
+import io
 
 
 def unique_symops():
 	f = open("symops_from_bilbao.txt", "r")
 	symops = set()
 	for line in f:
+		line = line.rstrip("\n")
 		if line not in symops:
 			symops.add(line)
-			sys.stdout.write(line)
+			print line
 	f.close()
 
 
@@ -18,5 +19,25 @@ def extract_from_dump():
 			print line[5:-8]
 	f.close()
 
+
+def compare_symops():
+	f = open("unique_symops_from_mantid.txt", "r")
+	mantidSymOps = set()
+	for line in f:
+		line = line.rstrip("\n")
+		mantidSymOps.add(line)
+	f.close()
+	
+	f = open("unique_symops_from_bilbao.txt", "r")
+	bilbaoSymOps = set()
+	for line in f:
+		line = line.rstrip("\n")
+		bilbaoSymOps.add(line)
+	f.close()
+
+	for e in mantidSymOps - bilbaoSymOps:
+		print e
+
 #extract_from_dump()
 #unique_symops()
+compare_symops()
