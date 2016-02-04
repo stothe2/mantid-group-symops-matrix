3c7982ae22ce5168700b6c538ffcf46d2e9ed286
asche1 <ascheie1@jhu.edu>
2016-02-04 09:16:00 -0500

    Update SpaceGroupSymOps.py

--- SpaceGroupSymOps.py
+++ SpaceGroupSymOps.py
@@ -95,6 +95,91 @@ dict_t = defaultdict(list)
 for k, v in t:
 	dict_t[k].append(v)
 
+# Symmetry operation list
+symList = ['x,y,z',
+	'-x,-y,-z',
+	'-x,y,-z',
+	'-x,y+1/2,-z',
+	'x+1/2,y+1/2,z',
+	'x,-y,z',
+	'x,-y,z+1/2',
+	'-x,y,-z+1/2',
+	'-x,y+1/2,-z+1/2',
+	'-x,-y,z',
+	'-x,-y,z+1/2',
+	'-x+1/2,y+1/2,-z',
+	'-x+1/2,-y,z+1/2',
+	'x,y+1/2,z+1/2',
+	'x+1/2,y,z+1/2',
+	'x+1/2,y+1/2,z+1/2',
+	'x+1/2,-y,z',
+	'x,-y+1/2,z+1/2',
+	'x+1/2,-y,z+1/2',
+	'x+1/2,-y+1/2,z',
+	'x+1/2,-y+1/2,z+1/2',
+	'x,-y+1/2,z',
+	'x+1/4,-y+1/4,z+1/4',
+	'-x+1/2,-y+1/2,z',
+	'-x+1/2,y,-z+1/2',
+	'-x+1/2,y,-z',
+	'-x+1/2,-y,z',
+	'-x+1/2,y+1/2,-z+1/2',
+	'-x+1/2,-y+1/2,z+1/2',
+	'-x,-y+1/2,z+1/2',
+	'-x,-y+1/2,z',
+	'-x+3/4,-y+3/4,z',
+	'-x+3/4,y,-z+3/4',
+	'-y,x,z',
+	'-y,x,z+1/4',
+	'-y,x,z+1/2',
+	'-y,x,z+3/4',
+	'-y,x+1/2,z+1/4',
+	'y,-x,-z',
+	'-y+1/2,x,z',
+	'-y,x+1/2,z+1/2',
+	'-y+3/4,x+1/4,z+1/4',
+	'-y+1/2,x+1/2,z',
+	'-y+1/2,x+1/2,z+1/4',
+	'-x+1/2,y+1/2,-z+1/4',
+	'-y+1/2,x+1/2,z+1/2',
+	'-y+1/2,x+1/2,z+3/4',
+	'-x+1/2,y+1/2,-z+3/4',
+	'-x+1/2,y,-z+3/4',
+	'-y+1/2,x,z+1/2',
+	'-y+1/4,x+3/4,z+1/4',
+	'-y,x-y,z',
+	'-y,x-y,z+1/3',
+	'-y,x-y,z+2/3',
+	'x+2/3,y+1/3,z+1/3',
+	'-y,-x,-z',
+	'y,x,-z',
+	'-y,-x,-z+2/3',
+	'-y,-x,-z+1/3',
+	'-y,-x,z',
+	'y,x,z',
+	'-y,-x,z+1/2',
+	'y,x,z+1/2',
+	'-y,-x,-z+1/2',
+	'y,x,-z+1/2',
+	'x,y,-z',
+	'y,x,-z+1/3',
+	'y,x,-z+2/3',
+	'x,y,-z+1/2',
+	'z,x,y',
+	'y+1/2,x+1/2,-z+1/2',
+	'y+3/4,x+1/4,-z+3/4',
+	'y+1/4,x+3/4,-z+3/4',
+	'y+3/4,x+1/4,-z+1/4',
+	'y+1/2,x+1/2,z+1/2',
+	'y+1/4,x+1/4,z+1/4',
+	'y+1/2,x+1/2,-z',
+	'-x+3/4,-y+1/4,z+1/2',
+	'-x+1/4,y+1/2,-z+3/4',
+	'y+3/4,x+1/4,-z+1/2',
+	'-x+1/4,-y+3/4,z+1/2',
+	'-x+3/4,y+1/2,-z+1/4',
+	'y+3/4,x+1/4,-z',
+]
 
 class SpaceGroupSymOps(PythonAlgorithm):
 
@@ -102,36 +187,44 @@ class SpaceGroupSymOps(PythonAlgorithm):
 		# ------------------------- Input properties -------------------------
 
 		# Space group and symmetry properties
-		self.declareProperty('SpaceGroup', 198, IntBoundedValidator(lower=1, upper=230),
+		self.declareProperty('Symmetrization by', 'Space Group', validator=StringListValidator(['Space Group', 'Symmetry Operations']))
+		self.declareProperty('Space Group', 198, IntBoundedValidator(lower=1, upper=230),
 			doc='Space group number as given in International Tables for Crystallography, Vol. A')
-		self.declareProperty('SymmetryOperations', 'All', validator=StringListValidator(['All', 'Choose']),
-			doc='Symmetry operations to be used on data')
-		self.declareProperty('-x+1/2,-y,z+1/2', True)
-		self.declareProperty('-x,y+1/2,-z+1/2', True)
-		self.declareProperty('z,x,y', True)
+		self.declareProperty('Number of symmetry operations', '1', validator=IntListValidator(['1', '2', '3', '4', '5']))
+		self.declareProperty('SymmetryList1', 'x,y,z', validator=StringListValidator(symList))
+		self.declareProperty('SymmetryList2', 'x,y,z', validator=StringListValidator(symList))
+		self.declareProperty('SymmetryList3', 'x,y,z', validator=StringListValidator(symList))
+		self.declareProperty('SymmetryList4', 'x,y,z', validator=StringListValidator(symList))
+		self.declareProperty('SymmetryList5', 'x,y,z', validator=StringListValidator(symList))
 
-		#self.setPropertySettings('-x+1/2,-y,z+1/2', EnabledWhenProperty('SymmetryOperations', PropertyCriterion.IsNotEqualTo, 'All'))
-		self.setPropertySettings('-x+1/2,-y,z+1/2', VisibleWhenProperty('SpaceGroup', PropertyCriterion.IsEqualTo, '198'))
-		self.setPropertySettings('-x,y+1/2,-z+1/2', VisibleWhenProperty('SpaceGroup', PropertyCriterion.IsEqualTo, '198'))
-		self.setPropertySettings('z,x,y', VisibleWhenProperty('SpaceGroup', PropertyCriterion.IsEqualTo, '198'))
+		self.setPropertySettings('Space Group', VisibleWhenProperty('Symmetrization by', PropertyCriterion.IsEqualTo, 'Space Group'))
+		self.setPropertySettings('Number of symmetry operations', VisibleWhenProperty('Symmetrization by', PropertyCriterion.IsEqualTo, 'Symmetry Operations'))
+		self.setPropertySettings('SymmetryList1', VisibleWhenProperty('Symmetrization by', PropertyCriterion.IsEqualTo, 'Symmetry Operations'))
+		self.setPropertySettings('SymmetryList2', VisibleWhenProperty('Number of symmetry operations', PropertyCriterion.IsMoreOrEqual, '2'))
+		self.setPropertySettings('SymmetryList3', VisibleWhenProperty('Number of symmetry operations', PropertyCriterion.IsMoreOrEqual, '3'))
+		self.setPropertySettings('SymmetryList4', VisibleWhenProperty('Number of symmetry operations', PropertyCriterion.IsMoreOrEqual, '4'))
+		self.setPropertySettings('SymmetryList5', VisibleWhenProperty('Number of symmetry operations', PropertyCriterion.IsMoreOrEqual, '5'))
 
-		sym_grp = 'Space group options'
-		self.setPropertyGroup('SymmetryOperations', sym_grp)
-		self.setPropertyGroup('SpaceGroup', sym_grp)
-		self.setPropertyGroup('-x+1/2,-y,z+1/2', sym_grp)
-		self.setPropertyGroup('-x,y+1/2,-z+1/2', sym_grp)
-		self.setPropertyGroup('z,x,y', sym_grp)
+		sym_grp = 'Symmetrization options'
+		self.setPropertyGroup('Symmetrization by', sym_grp)
+		self.setPropertyGroup('Space Group', sym_grp)
+		self.setPropertyGroup('Number of symmetry operations', sym_grp)
+		self.setPropertyGroup('SymmetryList1', sym_grp)
+		self.setPropertyGroup('SymmetryList2', sym_grp)
+		self.setPropertyGroup('SymmetryList3', sym_grp)
+		self.setPropertyGroup('SymmetryList4', sym_grp)
+		self.setPropertyGroup('SymmetryList5', sym_grp)
 
 		# Binning properties
 		self.declareProperty('BasisVector0', 'a,unit,1,1,0,0', StringMandatoryValidator(), 'Format: \'name,units,x,y,z\'')
 		self.declareProperty('BasisVector1', 'b,unit,0,0,1,0', StringMandatoryValidator(), 'Format: \'name,units,x,y,z\'')
-		self.declareProperty('AxisAligned', False, 'Perform binning aligned with the axes of the input MDEventWorkspace?')
-		self.declareProperty('NormalizedBasisVectors', True, 'Normalize the given basis vectors to unity')
-		self.declareProperty(FloatArrayProperty(name='OutputExtents',
+		self.declareProperty('Axis Aligned', False, 'Perform binning aligned with the axes of the input MDEventWorkspace?')
+		self.declareProperty('Normalized Basis Vectors', True, 'Normalize the given basis vectors to unity')
+		self.declareProperty(FloatArrayProperty(name='Output Extents',
 												values=[-5,8,-5,8],
 												validator=FloatArrayLengthValidator(4)),
 			'The minimum, maximum edges of space of each dimension of the OUTPUT workspace, as a comma-separated list')
-		self.declareProperty(FloatArrayProperty(name='OutputBins',
+		self.declareProperty(FloatArrayProperty(name='Output Bins',
 												values=[50,50],
 												validator=FloatArrayLengthValidator(2)),
 			'The number of bins for each dimension of the OUTPUT workspace')
@@ -139,43 +232,43 @@ class SpaceGroupSymOps(PythonAlgorithm):
 												values=[0,0,0,0],
 												validator=FloatArrayLengthValidator(4)),
 			'Coordinates in the INPUT workspace that corresponds to (0,0,0) in the OUTPUT workspace')
-		self.declareProperty(WorkspaceProperty(name='InputWorkspace',
+		self.declareProperty(WorkspaceProperty(name='Input Workspace',
 												defaultValue='',
 												direction=Direction.Input), 'An input MDWorkspace')
 
 		bin_grp = 'Binning parameters'
 		self.setPropertyGroup('BasisVector0', bin_grp)
 		self.setPropertyGroup('BasisVector1', bin_grp)
-		self.setPropertyGroup('AxisAligned', bin_grp)
-		self.setPropertyGroup('NormalizedBasisVectors', bin_grp)
-		self.setPropertyGroup('OutputExtents', bin_grp)
-		self.setPropertyGroup('OutputBins', bin_grp)
+		self.setPropertyGroup('Axis Aligned', bin_grp)
+		self.setPropertyGroup('Normalized Basis Vectors', bin_grp)
+		self.setPropertyGroup('Output Extents', bin_grp)
+		self.setPropertyGroup('Output Bins', bin_grp)
 		self.setPropertyGroup('Translation', bin_grp)
 
 		# ------------------------- Output properties ------------------------
-		self.declareProperty(WorkspaceProperty(name='BinnedWorkspace',
+		self.declareProperty(WorkspaceProperty(name='Binned Workspace',
 												defaultValue='',
 												direction=Direction.Output), 'A name for the output MDHistoWorkspace')
 
 	def PyExec(self):
-		sgNumber = self.getProperty("SpaceGroup").value
-		mdws = self.getProperty("InputWorkspace").value
-		basis0 = self.getProperty("BasisVector0").value
-		basis1 = self.getProperty("BasisVector1").value
-		axisAligned = self.getProperty("AxisAligned").value
-		normalizeBasisVectors = self.getProperty("NormalizedBasisVectors").value
-		outputExtents = self.getProperty("OutputExtents").value
-		outputBins = self.getProperty("OutputBins").value
-		translation = self.getProperty("Translation").value
+		sgNumber = self.getProperty('Space Group').value
+		mdws = self.getProperty('Input Workspace').value
+		basis0 = self.getProperty('BasisVector0').value
+		basis1 = self.getProperty('BasisVector1').value
+		axisAligned = self.getProperty('Axis Aligned').value
+		normalizeBasisVectors = self.getProperty('Normalized Basis Vectors').value
+		outputExtents = self.getProperty('Output Extents').value
+		outputBins = self.getProperty('Output Bins').value
+		translation = self.getProperty('Translation').value
+		symChoice = self.getProperty('Symmetrization by')
 
 		binned_ws = BinMD(InputWorkspace=mdws, AxisAligned=axisAligned,
 					BasisVector0=basis0, BasisVector1=basis1,
 					NormalizeBasisVectors=normalizeBasisVectors, Translation=translation,
 					OutputExtents=outputExtents, OutputBins=outputBins)
 
-		unit0, basisVec0 = self.__destringify(basis0)
-		unit1, basisVec1 = self.__destringify(basis1)
-
+		unit0, basisVec0 = self._destringify(basis0)
+		unit1, basisVec1 = self._destringify(basis1)
 
 		for index, item in enumerate(dict_sg[sgNumber]):
 			newBasisVec0 = dot(item, basisVec0)
@@ -192,17 +285,21 @@ class SpaceGroupSymOps(PythonAlgorithm):
 					NormalizeBasisVectors=normalizeBasisVectors, Translation=newTranslation,
 					OutputExtents=outputExtents, OutputBins=outputBins)
 
-		self.setProperty("BinnedWorkspace", binned_ws)
+		self.setProperty("Binned Workspace", binned_ws)
 
 
 	def category(self):
 		return 'PythonAlgorithms'
 
-	def __destringify(self, basis):
+	def _destringify(self, basis):
 		temp = basis.split(',')
 		unit = temp[0:2]
 		temp = temp[2:-1]
 		return unit, array([int(temp[0]), int(temp[1]), int(temp[2])])
+
+	def _symmetrize_by_generators(self, mdws, axisAligned, basis0, basis1,
+		normalizeBasisVectors, translation, outputExtents, outputBins):
+		pass
 
 
 # Register algorithm with Mantid
